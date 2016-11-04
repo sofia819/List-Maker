159db743c71276cc1876dc5e0ab0fa8351a6f168
Sofia Lee <lees13@wit.edu>
2016-11-04 14:43:20 -0400

    Base class written
    Item: Creates an item with the name and the amount.
    ListMaker: Adds item to list.
    ListTest: Uses ListMaker and item.

---
+++ src/ListTest.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class ListTest {
+	private static Scanner in = new Scanner(System.in);
+	private static ListMaker list = new ListMaker();
+	
+	public static void main(String[] args) throws IOException{
+		
+		readInput();
+		list.writeFile();
+		System.out.println(list);
+	}
+	
+	public static void readInput(){
+		while(true){
+			System.out.println("Please enter the item name (oneWord) and amount (-1 to stop): ");
+			String name = in.next();
+			if(name.equals("-1"))
+				break;
+			int amount = in.nextInt();
+			System.out.println(list.get(list.addItem(new Item(name, amount) ) ) );
+		}
+	}
+	
+	public static void readFile(String fileName) throws FileNotFoundException{
+		Scanner fin = new Scanner (new File(fileName));
+		while(fin.hasNextLine()){
+			String[] arr = fin.nextLine().split(" ");
+			String name = "";
+				if(arr.length > 1){
+					for(int i = 0; i < arr.length - 1; i++){
+						name += arr[i];
+					}
+				int amount = Integer.parseInt(arr[arr.length - 1]);
+				list.get(list.addItem(new Item(name, amount) ) );
+			}
+		}
+	}
+}
